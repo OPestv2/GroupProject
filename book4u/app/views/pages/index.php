32a8b45738a6bb114c9cd3ad98da580f32413732
OPestv2 <j.sieczka99@wp.pl>
2021-03-30 14:15:10 +0200

    create web folder

---
+++ book4u/app/views/pages/index.php
@@ -0,0 +1,6 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+    <h1><?php echo $data['title'].'<br>'; ?></h1>
+    <p>This is TraversyMVC PHP framework. Please refer to the docs on how to use it.</p>
+    
+<?php require APPROOT . '/views/inc/footer.php'; ?>
