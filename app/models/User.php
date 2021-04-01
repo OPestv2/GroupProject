96c300be631e3a289377aef43e5292fbbd3c0218
rsiejka <64097231+rsiejka@users.noreply.github.com>
2021-04-01 20:19:22 +0200

    user.php model

--- app/models/User.php
+++ app/models/User.php
@@ -21,21 +21,55 @@
 		}
 		
 		//sprawdzanie czy użytkownik istnieje
-		public function userexist($login){
+		public function userexist($login, $email){
+			$this->db->query('SELECT login, email FROM Users WHERE login = :login OR email = :email');
+			$this->db->bind(':login', $login);
+			$this->db->bind(':email', $email);
+			$user = $this->db->single();
 			
+			if($user != NULL){
+				return false;
+			}else{
+				return true;
+			}
 		}
 		
 		//rejestracja
-		public function register(){
-			
+		public function register($login, $email, $password_hash, $first_name, $last_name, $phone_number, $birth_date){
+			if(!userexist($login, $email)){
+				$this->db->query('INSERT INTO Users (password_hash, login, email, first_name, last_name, phone_number, birth_date) VALUES (:password_hash, :login, :email, :first_name, :last_name, :phone_number, :birth_date)');
+				$this->db->bind(':login', $login);
+				$this->db->bind(':email', $email);
+				$this->db->bind(':password_hash', $password_hash);
+				$this->db->bind(':first_name', $first_name);
+				$this->db->bind(':last_name', $last_name);
+				$this->db->bind(':phone_number', $phone_number);
+				$this->db->bind(':birth_date', $birth_date);
+				$register = $this->db->single();
+			}else{
+				return false;
+			}
 		}
 		
-		//odzyskanie hasła
-		public function restore(){
-			
+	//	//odzyskanie hasła
+	//	public function restore(){
+	//		
+	//	}
+		
+		//zmiana nicku
+		public function changenickname($oldnickname, $newnickname){
+			$this->db->query('UPDATE Users SET login = :newnickname WHERE login = :oldnickname');
+			$this->db->bind(':oldnickname', $oldnickname);
+			$this->db->bind(':newnickname', $newnickname);
 		}
+
 		
-		
+		//zmiana hasła
+		public function changepassword($newpasswordhash, $login){
+			$this->db->query('UPDATE Users SET password_hash = :newpasswordhash WHERE login = :login');
+			$this->db->bind(':login', $login);
+			$this->db->bind(':newpasswordhash', $newpasswordhash);
+		}
 	}
 
 
