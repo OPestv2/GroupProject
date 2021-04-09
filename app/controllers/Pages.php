9c743c0b3f7c661d6adf8856170a4a20dfcd9e6f
rsiejka <64097231+rsiejka@users.noreply.github.com>
2021-04-09 18:24:13 +0200

    model multimedia update

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -16,6 +16,19 @@
 
             $this->view('pages/about', $data);
         }
+		
+		public function getBooks(){
+               $notes = $this->multimediaModel->getNotes();
+
+                $data = [
+                    'title' => 'Notatki',
+                    'description' => 'Dodaj nowa lub modyfikuj notatke',
+                    'notes' => $notes
+                ];
+
+                $this->view('users/notes',$data);
+        }
+		
 
         public function main(){
         	//pobranie ilosci Multimedia z bazy danych
